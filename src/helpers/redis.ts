ff09463048cef7dbc5b1722243c8c3336d6108dc
diepmyduong@outlook.com.vn <diepmyduong@outlook.com.vn>
2021-08-21 19:30:43 +0700

    Fix: Lỗi cấu hình pass redis

--- src/helpers/redis.ts
+++ src/helpers/redis.ts
@@ -4,7 +4,7 @@ import config from "config";
 const redis = Redis.createClient({
   host: config.get<string>("redis.host"),
   port: config.get<number>("redis.port"),
-  password: config.get<string>("redis.password"),
+  password: config.get<string>("redis.pass"),
   prefix: config.get<string>("redis.prefix"),
 });
 
